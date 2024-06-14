dd3f2ef31428d70e148cd5475435db24d21a36fe
Dhananjoy Das <dasdhananjoy733@gmail.com>
2024-06-14 12:46:29 +0530

    add: initial files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import Title from './components/Title/Title';
 import Gallery from './components/Gallery/Gallery';
@@ -8,7 +7,6 @@ import Review from './components/Review/Review';
 const App = () => {
   return (
     <div>
-      <Navbar/>
       <Hero/>
       <div className='container'>
        <Title subTitle='Gallery' title='Photo Gallery'/>
